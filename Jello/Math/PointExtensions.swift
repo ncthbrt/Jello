1ccfa378c5a8ceb5730d4e0521086e79e5f9dca6
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-01-31 18:25:38 +0200

    Started working on spline editor

--- Jello/Math/PointExtensions.swift
+++ Jello/Math/PointExtensions.swift
@@ -61,6 +61,10 @@ extension CGPoint {
     init(_ size: CGSize){
         self.init(x: size.width, y: size.height)
     }
+    
+    init(_ point: SplinePoint) {
+        self.init(x: CGFloat(point.x), y: CGFloat(point.y))
+    }
 }
 
 
@@ -94,5 +98,75 @@ extension vector_float2 {
     
     init(_ point: CGPoint) {
         self.init(x: Float(point.x), y: Float(point.y))
+    }
+}
+
+
+
+extension SplinePoint {
+    static func -(lhs: SplinePoint, rhs: SplinePoint) -> SplinePoint {
+        return SplinePoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    static func +(lhs: SplinePoint, rhs: SplinePoint) -> SplinePoint  {
+        return SplinePoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    static func *(lhs: SplinePoint, rhs: SplinePoint) -> SplinePoint  {
+        return SplinePoint(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+    }
+    
+    static func *(lhs: SplinePoint, rhs: Float) -> SplinePoint  {
+        return SplinePoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
+    static func /(lhs: SplinePoint, rhs: Float) -> SplinePoint  {
+        return SplinePoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+    
+    
+    static func /(lhs: SplinePoint, rhs: SplinePoint) -> SplinePoint  {
+        return SplinePoint(x: lhs.x / rhs.x, y: lhs.y / rhs.x)
+    }
+    
+    static func *(lhs: Float, rhs: SplinePoint) -> SplinePoint  {
+        return SplinePoint(x: lhs * rhs.x, y: lhs * rhs.y)
+    }
+    
+    func magnitude() -> Float {
+        return sqrtf(Float(x*x+y*y))
+    }
+
+    func magnitudeSquared() -> Float {
+        return x*x+y*y
+    }
+    
+    func setMagnitude(factor: Float) -> SplinePoint {
+        (self.normal()) * factor
+    }
+
+    func normal() -> SplinePoint {
+        let mag = self.magnitude()
+        if abs(mag) < 0.00001 {
+            return SplinePoint(x: 1, y: 0)
+        }
+        return SplinePoint(x: x / mag, y:  y / mag)
+    }
+    
+    static func lerp(a: SplinePoint, b: SplinePoint, t: Float) -> SplinePoint {
+        return (a * (1 - t)) + (t * b)
+    }
+    
+    init(_ point: vector_float2) {
+        self.init(x: Float(point.x), y: Float(point.y))
+    }
+    
+    
+    init(_ point: CGPoint) {
+        self.init(x: Float(point.x), y: Float(point.y))
+    }
+    
+    init(_ size: CGSize){
+        self.init(x: Float(size.width), y: Float(size.height))
     }
 }
